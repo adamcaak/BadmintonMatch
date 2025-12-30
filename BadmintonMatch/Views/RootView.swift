7bb964477f37caaad76c3619120c05917def3a71
adamcaak <ladamczak1997@gmail.com>
2025-12-30 20:00:10 +0100

    Sprint 0
    Create project and file structure, add new file RootView

---
+++ BadmintonMatch/Views/RootView.swift
@@ -0,0 +1,19 @@
+//
+//  RootView.swift
+//  BadmintonMatch
+//
+//  Created by Łukasz Adamczak on 30/12/2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Root View")
+            .font(.title)
+    }
+}
+
+#Preview {
+    RootView()
+}
