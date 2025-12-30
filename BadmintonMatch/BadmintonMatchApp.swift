a5d94a1bf826a8520eec1425d6457669d38ef924
adamcaak <ladamczak1997@gmail.com>
2025-12-30 19:50:34 +0100

    Initial Commit

---
+++ BadmintonMatch/BadmintonMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadmintonMatchApp.swift
+//  BadmintonMatch
+//
+//  Created by Łukasz Adamczak on 30/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BadmintonMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BadmintonMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BadmintonMatch
+//
+//  Created by Łukasz Adamczak on 30/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
